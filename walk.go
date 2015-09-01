fbdb501a0c0846607015e185bb3414d51e384d92
Shelikhoo <xiaokangwang@outlook.com>
2015-09-01 16:00:03 +0800

    finish LimitedSizeReadFrom

--- walk.go
+++ walk.go
@@ -92,14 +92,66 @@ func (lf *LimitedSizeWriteToFile) Write(p []byte) (n int, err error) {
 		}
 	}
 
-	n, err := cfd.Write(p)
+	n, err := lf.cfd.Write(p)
+
+	lf.cfnd += n
+	lf.nd += n
 
 	return n, err
 
 }
 
 func (lf *LimitedSizeWriteToFile) Finialize() (FileCreated, LastSize, TotalBytesWritten int64) {
-	cfd.Close()
+	lf.cfd.Close()
+	return cfn, cfnd, nd
+
+}
+
+type LimitedSizeReadFrom struct {
+	io.Reader
+	TargetPatten string
+	cfd          *os.File //current file descripter
+	cfn          int64    //currnet file number
+	cfnd         int64    //current file byte readden
+	nd           int64    //bytes readden
+}
+
+func (lf *LimitedSizeReadFrom) Read(p []byte) (n int, err error) {
+	if lf.TargetPatten == "" {
+		return 0, errors.New("LimitedSizeWriteToFile: no patten set")
+	}
+
+	//Open a file if first call
+	if lf.nd == 0 {
+		fn := fmt.Sprintf(lf.TargetPatten, cfn)
+		lf.fd, err = os.OpenFile(fn)
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	n, err := lf.cfd.Read(p)
+
+	if err == io.EOF {
+		fn := fmt.Sprintf(lf.TargetPatten, cfn+1)
+		lf.fd, err = os.OpenFile(fn)
+		if err != nil {
+
+			if os.IsNotExist(err) {
+				return 0, io.EOF
+			} else {
+				return 0, err
+			}
+			cfn += 1
+			cfnd = 0
+		}
+	}
+	return n, err
+
+}
+
+func (lf *LimitedSizeReadFrom) Finialize() (FileCreated, LastSize, TotalBytesWritten int64) {
+	lf.cfd.Close()
 	return cfn, cfnd, nd
 
 }
