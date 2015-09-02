20c77890e0709bdc1d2d9e8685d4bd593110c272
Shelikhoo <xiaokangwang@outlook.com>
2015-09-02 09:23:06 +0800

    partly: reverse

--- walk.go
+++ walk.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"archive/tar"
+	"bytes"
+	"encoding/binary"
 	"errors"
 	"github.com/codahale/chacha20"
 	"github.com/golang/snappy"
@@ -13,8 +15,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strconv"
-  "bytes"
-  "encoding/binary"
+	"url"
 )
 
 func progd_forword(ar cmdoptS) {
@@ -97,7 +98,7 @@ func progd_forword(ar cmdoptS) {
 
 	}
 
-  _,_,nd=LimitedSizeWriteToFilei.Finialize()
+	_, _, nd = LimitedSizeWriteToFilei.Finialize()
 
 	FileHash := new([]byte, 64)
 	HashWriter.Read(FileHash)
@@ -115,16 +116,14 @@ func progd_forword(ar cmdoptS) {
 		os.Exit(-1)
 	}
 
-  bb:=new(bytes.Buffer)
-  binary.Write(bb,binary.LittleEndian,nd)
+	bb := new(bytes.Buffer)
+	binary.Write(bb, binary.LittleEndian, nd)
 
-
-
-  err = db.Put([]byte("packagesum"), bb.Bytes() , nil)
-  if err != nil {
-    fmt.Println(err.Error())
-    os.Exit(-1)
-  }
+	err = db.Put([]byte("packagesum"), bb.Bytes(), nil)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(-1)
+	}
 
 	//we won't use it anymore
 	db.Close()
@@ -153,146 +152,146 @@ func progd_forword(ar cmdoptS) {
 	}
 
 	fmt.Println("Hash: %x", FileHash)
-  fmt.Println("Key: %s", ar.secret_key)
+	fmt.Println("Key: %s", ar.secret_key)
 
 }
 
 func progd_reverse(ar cmdoptS) {
 
-  if ar.parrate != 0 { //we do not care the actual number
-    path, err := exec.LookPath("par2")
-    if err != nil {
-      fmt.Println("Unable to whereis par2, metadata reconstruction was ignored:" + err.Error())
-    }
+	if ar.parrate != 0 { //we do not care the actual number
+		path, err := exec.LookPath("par2")
+		if err != nil {
+			fmt.Println("Unable to whereis par2, metadata reconstruction was ignored:" + err.Error())
+		}
 
-    DirIf, _ := os.Open(ar.out_dir)
-    DirIfs, _ := DirIf.Readdirnames()
+		DirIf, _ := os.Open(ar.out_dir)
+		DirIfs, _ := DirIf.Readdirnames()
 
-    cmd := exec.Command("par2", "r", "-a"+"mdpp", "-v", "--", "md")
-    Absp, _ := filepath.Abs(ar.out_dir)
-    cmd.Dir = Absp
-    err = cmd.Start()
-    if err != nil {
-      fmt.Println("Unable to exec par2, metadata reconstruction data compute was ignored:" + err.Error())
-    }
-    err = cmd.Wait()
-    if err != nil {
-      fmt.Println("par2 was finished unsuccessfully, metadata reconstruction data compute was ignored(or failed):" + err.Error())
-    }
-  }
+		cmd := exec.Command("par2", "r", "-a"+"mdpp", "-v", "--", "md")
+		Absp, _ := filepath.Abs(ar.out_dir)
+		cmd.Dir = Absp
+		err = cmd.Start()
+		if err != nil {
+			fmt.Println("Unable to exec par2, metadata reconstruction data compute was ignored:" + err.Error())
+		}
+		err = cmd.Wait()
+		if err != nil {
+			fmt.Println("par2 was finished unsuccessfully, metadata reconstruction data compute was ignored(or failed):" + err.Error())
+		}
+	}
 
-  //Open metadata leveldb
-  db, err := leveldb.OpenFile(ar.in_dir+"/md", nil)
+	//Open metadata leveldb
+	db, err := leveldb.OpenFile(ar.in_dir+"/md", nil)
 
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(-1)
 	}
 
-  ndb,err:=db.Get([]byte("packagesum"),nil)
+	ndb, err := db.Get([]byte("packagesum"), nil)
 
-  if err != nil {
-    fmt.Println(err.Error())
-    os.Exit(-1)
-  }
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(-1)
+	}
 
-  var nd int64
-  var not_going bool
+	var nd int64
+	var not_going bool
 
-  missing_file:=make([]string,0,25)
-  all_file:=make([]string,0,25)
+	missing_file := make([]string, 0, 25)
+	all_file := make([]string, 0, 25)
 
-  binary.Read(ndb,binary.LittleEndian,nd)
+	binary.Read(ndb, binary.LittleEndian, nd)
 
-  for cfn := range(nd){
-    cnnn:=fmt.Sprintf(ar.in_dir + "/df%X", cfn)
-    append(all_file,fmt.Sprintf("df%X", cfn))
+	for cfn := range nd {
+		cnnn := fmt.Sprintf(ar.in_dir+"/df%X", cfn)
+		append(all_file, fmt.Sprintf("df%X", cfn))
 
-    if _, err := os.Stat(cnnn); err != nil {
-      if ar.parrate==0{
-        not_going=true
-        append(missing_file,fmt.Sprintf("df%X", cfn))
-      }else{
+		if _, err := os.Stat(cnnn); err != nil {
+			if ar.parrate == 0 {
+				not_going = true
+				append(missing_file, fmt.Sprintf("df%X", cfn))
+			} else {
 
-        //touch the missing file so that par2 will try to recover this
-        cfnd,err:=os.Create(cnnn)
+				//touch the missing file so that par2 will try to recover this
+				cfnd, err := os.Create(cnnn)
 
-        if err != nil {
-          fmt.Println(err.Error())
-          os.Exit(-1)
-        }
+				if err != nil {
+					fmt.Println(err.Error())
+					os.Exit(-1)
+				}
 
-        cfnd.Close()
+				cfnd.Close()
 
-        append(missing_file,fmt.Sprintf("df%X", cfn))
+				append(missing_file, fmt.Sprintf("df%X", cfn))
 
-      }
-    }
-  }
+			}
+		}
+	}
 
-  if len(missing_file)!=0{
-    if ar.parrate==0{
-      fmt.Println("%d file missing")
+	if len(missing_file) != 0 {
+		if ar.parrate == 0 {
+			fmt.Println("%d file missing")
 
-      for cf:=range(len(missing_file)){
-        fmt.Println(cf)
-      }
+			for cf := range len(missing_file) {
+				fmt.Println(cf)
+			}
 
-      fmt.Println("Failed to reverse operate as there is file missing.")
-      os.Exit(-1)
+			fmt.Println("Failed to reverse operate as there is file missing.")
+			os.Exit(-1)
 
-    }else{
-      fmt.Println("%d file missing, but reconstruction by par2 underway.")
+		} else {
+			fmt.Println("%d file missing, but reconstruction by par2 underway.")
 
-      for cf:=range(len(missing_file)){
-        fmt.Println(missing_file[cf])
-      }
-    }
-  }
+			for cf := range len(missing_file) {
+				fmt.Println(missing_file[cf])
+			}
+		}
+	}
 
-data_reconstruction_unsuccessful:=true
+	data_reconstruction_unsuccessful := true
 
-  if ar.parrate != 0 { //we do not care the actual number
-    path, err := exec.LookPath("par2")
-    if err != nil {
-      fmt.Println("Unable to whereis par2, data reconstruction was ignored:" + err.Error())
-    }
+	if ar.parrate != 0 { //we do not care the actual number
+		path, err := exec.LookPath("par2")
+		if err != nil {
+			fmt.Println("Unable to whereis par2, data reconstruction was ignored:" + err.Error())
+		}
 
-    cmd := exec.Command("par2", "r", "-a"+"mdpp", "-v", "--", all_file...)
-    Absp, _ := filepath.Abs(ar.out_dir)
-    cmd.Dir = Absp
-    err = cmd.Start()
-    if err != nil {
-      fmt.Println("Unable to exec par2, metadata reconstruction was ignored:" + err.Error())
-    }
-    err = cmd.Wait()
-    if err != nil {
-      fmt.Println("par2 was finished unsuccessfully, data reconstruction was ignored(or failed):" + err.Error())
-    }else{
-      data_reconstruction_unsuccessful=false
-    }
-  }
+		cmd := exec.Command("par2", "r", "-a"+"mdpp", "-v", "--", all_file...)
+		Absp, _ := filepath.Abs(ar.out_dir)
+		cmd.Dir = Absp
+		err = cmd.Start()
+		if err != nil {
+			fmt.Println("Unable to exec par2, metadata reconstruction was ignored:" + err.Error())
+		}
+		err = cmd.Wait()
+		if err != nil {
+			fmt.Println("par2 was finished unsuccessfully, data reconstruction was ignored(or failed):" + err.Error())
+		} else {
+			data_reconstruction_unsuccessful = false
+		}
+	}
 
-  if ar.parrate != 0 && data_reconstruction_unsuccessful {
-    fmt.Println("operation failed: unable to reconstruct.")
-    fmt.Println("If data were correct, remove parrate might do.")
+	if ar.parrate != 0 && data_reconstruction_unsuccessful {
+		fmt.Println("operation failed: unable to reconstruct.")
+		fmt.Println("If data were correct, remove parrate might do.")
 
-    for dfnow:= range(len(missing_file)){
-      os.Remove(ar.in_dir+"/"+missing_file)
-    }
+		for dfnow := range len(missing_file) {
+			os.Remove(ar.in_dir + "/" + missing_file)
+		}
 
-    os.Exit(-1)
-  }
+		os.Exit(-1)
+	}
 
-  //now we do the actual job
+	//now we do the actual job
 
-  nonce,err := db.Get([]byte("nonce"), nil)
-  if err != nil {
-    fmt.Println(err.Error())
-    os.Exit(-1)
-  }
+	nonce, err := db.Get([]byte("nonce"), nil)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(-1)
+	}
 
-  //calc key
+	//calc key
 
 	keyhasher := sha3.NewShake256()
 
@@ -305,8 +304,56 @@ data_reconstruction_unsuccessful:=true
 	poly1305key := make([]byte, 32)
 	keyhasher.Read(poly1305key)
 
+	//set up stream
+
+	var LimitedSizeReadFromi LimitedSizeReadFrom
+
+	LimitedSizeReadFromi.TargetPatten = ar.in_dir + "/df%X"
+
+	cryptos, err := chacha20.NewXChaCha(xchachakey, nonce)
+
+	HashWriter := sha3.NewShake256()
+
+	Tread := io.TeeReader(LimitedSizeReadFromi, HashWriter)
+
+	DataReader := NewDecryptedReader(Tread, cryptos)
+
+	DeCompressedStream = snappy.NewReader(DataReader)
+
+	TarStream := tar.NewReader(DeCompressedStream)
+
+	for {
+		hdr, err := TarStream.Next()
+		if err == io.EOF {
+			// end of tar archive
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+		filenamex := hdr.Name
+		if !IsPathAllowed(hdr.Name) {
+			filenamex = url.QueryEscape(hdr.Name)
+		}
+
+		os.Create(ar.out_dir + "/" + filenamex)
+
+	}
+
 }
 
+func IsPathAllowed(p string) bool {
+	if filepath.IsAbs(p) == true {
+		return false
+	}
+
+	if strings.Contains(filepath.ToSlash(filepath.Clean(p)), "../") {
+		return false
+	}
+
+	return true
+
+}
 
 func GenFileList(s string) {
 
